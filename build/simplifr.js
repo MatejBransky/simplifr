321e17c9881bd78e608d49ac2283c28a42577a9b
krispo <konstantin.skipor@gmail.com>
2016-03-04 21:12:55 +0300

    change version

--- build/simplifr.js
+++ build/simplifr.js
@@ -1,4 +1,4 @@
-/* Simplifr, v0.0.5 */
+/* Simplifr, v0.0.6 */
 (function (global, factory) {
   typeof exports === 'object' && typeof module !== 'undefined' ? factory(exports) :
   typeof define === 'function' && define.amd ? define(['exports'], factory) :
