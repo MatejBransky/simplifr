314f6399e7541d69ca53087c029728b00d20b893
krispo <konstantin.skipor@gmail.com>
2016-03-10 15:07:59 +0300

    added more tests

--- test/simplify-test.js
+++ test/simplify-test.js
@@ -67,4 +67,53 @@ test('-= simplify tests =-', function(t){
     t.end();
   });
 
+  t.test('simplify complex data with nested arrays ', function(t){
+    var json = {
+      foo: {
+        bar: [
+          { buz: [1, 2, 3] },
+          { tat: [ { a: 1 }, { b: 2 }, { c: 3 } ] },
+          { sat: [ [1, 2, 3], [4, 5, 6], [7, 8, 9] ] },
+        ]
+      }
+    };
+    var res = {
+      'root': { type: 'object', childs: ['foo']},
+      'root.foo': { type: 'object', childs: ['bar']},
+      'root.foo.bar': { type: 'array', childs: [0, 1, 2]},
+
+      'root.foo.bar.0': { type: 'object', childs: ['buz']},
+      'root.foo.bar.0.buz': { type: 'array', childs: [0, 1, 2]},
+      'root.foo.bar.0.buz.0': 1,
+      'root.foo.bar.0.buz.1': 2,
+      'root.foo.bar.0.buz.2': 3,
+
+      'root.foo.bar.1': { type: 'object', childs: ['tat']},
+      'root.foo.bar.1.tat': { type: 'array', childs: [0, 1, 2]},
+      'root.foo.bar.1.tat.0': { type: 'object', childs: ['a']},
+      'root.foo.bar.1.tat.0.a': 1,
+      'root.foo.bar.1.tat.1': { type: 'object', childs: ['b']},
+      'root.foo.bar.1.tat.1.b': 2,
+      'root.foo.bar.1.tat.2': { type: 'object', childs: ['c']},
+      'root.foo.bar.1.tat.2.c': 3,
+
+      'root.foo.bar.2': { type: 'object', childs: ['sat']},
+      'root.foo.bar.2.sat': { type: 'array', childs: [0, 1, 2]},
+      'root.foo.bar.2.sat.0': { type: 'array', childs: [0, 1, 2]},
+      'root.foo.bar.2.sat.0.0': 1,
+      'root.foo.bar.2.sat.0.1': 2,
+      'root.foo.bar.2.sat.0.2': 3,
+      'root.foo.bar.2.sat.1': { type: 'array', childs: [0, 1, 2]},
+      'root.foo.bar.2.sat.1.0': 4,
+      'root.foo.bar.2.sat.1.1': 5,
+      'root.foo.bar.2.sat.1.2': 6,
+      'root.foo.bar.2.sat.2': { type: 'array', childs: [0, 1, 2]},
+      'root.foo.bar.2.sat.2.0': 7,
+      'root.foo.bar.2.sat.2.1': 8,
+      'root.foo.bar.2.sat.2.2': 9
+    };
+    t.deepEqual(simplify(json), res);
+    t.end();
+  });
+
 });
